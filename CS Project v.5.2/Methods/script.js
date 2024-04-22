9442e0c6027a927707f78d9227f007ca05ecbe72
SheepDogRaph <166275111+SheepDogRaph@users.noreply.github.com>
2024-04-22 21:04:26 +0800

    Add files via upload

---
+++ CS Project v.5.2/Methods/script.js
@@ -0,0 +1,12 @@
+
+
+$(document).ready(function(){
+
+    $("#collapse").on("click",function(){
+
+        $("#sidebar").toggleClass("active");
+        $(".fa-align-left").toggleClass("fa-align-right");
+
+    })
+
+})
---
+++ CS Project v.5.2/script.js
@@ -0,0 +1,13 @@
+
+
+$(document).ready(function(){
+
+    $("#collapse").on("click",function(){
+
+        $("#sidebar").toggleClass("active");
+        $(".fa-align-left").toggleClass("fa-align-right");
+
+    })
+
+})
+
